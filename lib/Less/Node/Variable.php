3988b9933c331341bca87535944aaff6b775b39b
Josh <oyejorge@gmail.com>
2013-08-14 09:15:01 -0600

    self referencing variable gives a proper error (no stack overflow)

--- lib/Less/Node/Variable.php
+++ lib/Less/Node/Variable.php
@@ -7,6 +7,7 @@ class Variable {
 	public $name;
 	public $index;
 	public $file;
+	private $evaluating = false;
 
     public function __construct($name, $index, $file = null) {
         $this->name = $name;
@@ -20,12 +21,21 @@ class Variable {
             $v = new \Less\Node\Variable(substr($name, 1), $this->index + 1);
             $name = '@' . $v->compile($env)->value;
         }
+
+		if ($this->evaluating) {
+            throw new \Less\Exception\CompilerException("Recursive variable definition for " + $name, $this->index, null, $this->file);
+		}
+
+		$this->evaluating = true;
+
         $callback = function ($frame) use ($env, $name) {
             if ($v = $frame->variable($name)) {
                 return $v->value->compile($env);
             }
         };
+
         if ($variable = \Less\Environment::find($env->frames, $callback)) {
+			$this->evaluating = false;
             return $variable;
         } else {
             throw new \Less\Exception\CompilerException("variable " . $name . " is undefined", $this->index, null, $this->file);
