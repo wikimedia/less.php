0693114f36038c8bc55d436494a9a4bf9fa420d8
Josh <oyejorge@gmail.com>
2013-08-14 09:33:13 -0600

    add support for unicode descriptors

---
+++ lib/Less/Node/UnicodeDescriptor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Less\Node;
+
+class UnicodeDescriptor{
+
+	public function __construct($value){
+		$this->value = $value;
+	}
+
+	public function toCss($env){
+		return $this->value;
+	}
+
+	public function compile($env){
+		return $this;
+	}
+}
+
