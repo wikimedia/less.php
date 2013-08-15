95f22ae0998d11624910e7c536c129913cd95342
Josh <oyejorge@gmail.com>
2013-08-15 10:44:02 -0600

    correct handling of units

--- lib/Less/Node/Dimension.php
+++ lib/Less/Node/Dimension.php
@@ -2,12 +2,18 @@
 
 namespace Less\Node;
 
-class Dimension
-{
-    public function __construct($value, $unit = false)
-    {
+class Dimension{
+
+    public function __construct($value, $unit = false){
         $this->value = floatval($value);
-        $this->unit = $unit;
+
+		if( $unit && ($unit instanceof \Less\Node\Unit) ){
+			$this->unit = $unit;
+		}elseif( $unit ){
+			$this->unit = new \Less\Node\Unit( array($unit) );
+		}else{
+			$this->unit = new \Less\Node\Unit( );
+		}
     }
 
     public function compile($env = null) {
@@ -18,32 +24,64 @@ class Dimension
         return new \Less\Node\Color(array($this->value, $this->value, $this->value));
     }
 
-    public function toCSS()
-    {
-        return $this->value . $this->unit;
+    public function toCSS(){
+        return $this->unit->isEmpty() ? $this->value :
+			$this->value . $this->unit->toCSS();
     }
 
-    public function __toString()
-    {
+    public function __toString(){
         return $this->toCSS();
     }
 
     // In an operation between two Dimensions,
     // we default to the first Dimension's unit,
     // so `1px + 2em` will yield `3px`.
-    // In the future, we could implement some unit
-    // conversions such that `100cm + 10mm` would yield
-    // `101cm`.
-    public function operate($op, $other)
-    {
-        return new \Less\Node\Dimension( \Less\Environment::operate($op, $this->value, $other->value), $this->unit ?: $other->unit);
+    public function operate($op, $other){
+
+		$value = \Less\Environment::operate($op, $this->value, $other->value);
+		$unit = clone $this->unit;
+
+		if( $op === '+' || $op === '-' ){
+
+			if( !count($unit->numerator) && !count($unit->denominator) ){
+				$unit->numerator = $other->unit->numerator;
+				$unit->denominator = $other->unit->denominator;
+			}elseif( !count($other->unit->numerator) && !count($other->unit->denominator) ){
+				// do nothing
+			}else{
+				$other = $other->convertTo( $this->unit->usedUnits());
+
+				if( $other->unit->toCSS() != $unit->toCSS() ){
+					throw new \Less\Exception\CompilerException("Incompatible units '".$unit->toCSS() . "' and ".$other->unit->toCSS()+"'.");
+				}
+
+				$value = \Less\Environment::operate($op, $this->value, $other->value);
+			}
+		}elseif( $op === '*' ){
+			$unit->numerator = array_merge($unit->numerator, $other->unit->numerator);
+			$unit->denominator = array_merge($unit->denominator, $other->unit->denominator);
+			sort($unit->numerator);
+			sort($unit->denominator);
+			$unit->cancel();
+		}elseif( $op === '/' ){
+			$unit->numerator = array_merge($unit->numerator, $other->unit->denominator);
+			$unit->denominator = array_merge($unit->denominator, $other->unit->numerator);
+			sort($unit->numerator);
+			sort($unit->denominator);
+			$unit->cancel();
+		}
+		return new \Less\Node\Dimension( $value, $unit);
     }
 
 	public function compare($other) {
 		if ($other instanceof Dimension) {
-			if ($other->value > $this->value) {
+
+			$a = $this->unify()->value;
+			$b = $other->unify()->value;
+
+			if ($b > $a) {
 				return -1;
-			} elseif ($other->value < $this->value) {
+			} elseif ($b < $a) {
 				return 1;
 			} else {
 				if ($other->unit && $this->unit !== $other->unit) {
@@ -55,4 +93,45 @@ class Dimension
 			return -1;
 		}
 	}
+
+	function unify() {
+		return $this->convertTo(array('length'=> 'm', 'duration'=> 's' ));
+	}
+
+    function convertTo($conversions) {
+		$value = $this->value;
+		$unit = clone $this->unit;
+
+		foreach($conversions as $groupName => $targetUnit){
+			$group = \Less\Node\UnitConversions::$$groupName;
+
+			//numerator
+			for($i=0; $i < count($unit->numerator); $i++ ){
+				$atomicUnit = $this->numerator[$i];
+				if( !isset($group[$atomicUnit]) ){
+					continue;
+				}
+
+				$value = $value * ($group[$atomicUnit] / $group[$targetUnit]);
+
+				$unit->numerator[$i] = $targetUnit;
+			}
+
+			//denominator
+			for($i=0; $i < count($unit->denominator); $i++ ){
+				$atomicUnit = $this->denominator[$i];
+				if( !isset($group[$atomicUnit]) ){
+					continue;
+				}
+
+				$value = $value / ($group[$atomicUnit] / $group[$targetUnit]);
+
+				$unit->denominator[$i] = $targetUnit;
+			}
+		}
+
+		$unit->cancel();
+
+		return new \Less\Node\Dimension( $value, $unit);
+    }
 }
