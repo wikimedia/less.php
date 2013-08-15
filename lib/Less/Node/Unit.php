d00f56afb00fbd24ded59b812c12b1550bff3926
Josh <oyejorge@gmail.com>
2013-08-15 10:44:18 -0600

    correct handling of units

---
+++ lib/Less/Node/Unit.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Less\Node;
+
+class Unit{
+	var $numerator = array();
+	var $denominator = array();
+
+	function __construct($numerator = array(), $denominator = array()){
+		$this->numerator = $numerator;
+		$this->denominator = $denominator;
+		sort($this->numerator);
+		sort($this->denominator);
+	}
+
+	function toCSS(){
+		$css = implode('*',$this->numerator);
+		for( $i = 0; $i < count($this->denominator); $i++ ){
+			$css += '/'.$this->denominator[$i];
+		}
+
+		return $css;
+	}
+
+	function is($unitString){
+		return $this->toCSS() === $unitString;
+	}
+
+	function isEmpty(){
+		return count($this->numerator) === 0 && count($this->denominator) === 0;
+	}
+
+	function map($callback){
+
+		for($i=0; $i < count($this->numerator); $i++ ){
+			$this->numerator[$i] = call_user_func($callback, $this->numerator[$i],false);
+		}
+
+		for($i=0; $i < count($this->denominator); $i++ ){
+			$this->denominator[$i] = call_user_func($callback, $this->denominator[$i],false);
+		}
+
+	}
+
+	function usedUnits(){
+		$result = array();
+
+		foreach(\Less\Node\UnitConversions::$groups as $groupName){
+			$group = \Less\Node\UnitConversions::$$groupName;
+
+			for($i=0; $i < count($this->numerator); $i++ ){
+				$atomicUnit = $this->numerator[$i];
+				if( isset($group[$atomicUnit]) && !isset($result[$groupName]) ){
+					$result[$groupName] = $atomicUnit;
+				}
+			}
+
+			for($i=0; $i < count($this->denominator); $i++ ){
+				$atomicUnit = $this->denominator[$i];
+				if( isset($group[$atomicUnit]) && !isset($result[$groupName]) ){
+					$result[$groupName] = $atomicUnit;
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	function cancel(){
+		$counter = array();
+
+		for ($i = 0; $i < count($this->numerator); $i++) {
+			$atomicUnit = $this->numerator[$i];
+			$counter[$atomicUnit] = ( isset($counter[$atomicUnit]) ? $counter[$atomicUnit] : 0) + 1;
+		}
+
+		for ($i = 0; $i < count($this->denominator); $i++) {
+			$atomicUnit = $this->denominator[$i];
+			$counter[$atomicUnit] = ( isset($counter[$atomicUnit]) ? $counter[$atomicUnit] : 0) - 1;
+		}
+
+		$this->numerator = array();
+		$this->denominator = array();
+
+
+		foreach($counter as $atomicUnit => $count){
+			if( $count > 0 ){
+				for( $i = 0; $i < $count; $i++ ){
+					$this->numerator[] = $atomicUnit;
+				}
+			}elseif( $count < 0 ){
+				for( $i = 0; $i < -$count; $i++ ){
+					$this->denominator[] = $atomicUnit;
+				}
+			}
+		}
+
+		sort($this->numerator);
+		sort($this->denominator);
+	}
+
+
+}
+
---
+++ lib/Less/Node/UnitConversions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Less\Node;
+
+class UnitConversions{
+
+	static $groups = array('length','duration');
+
+	static $length = array(
+		'm'=> 1,
+		'cm'=> 0.01,
+		'mm'=> 0.001,
+		'in'=> 0.0254,
+		'pt'=> 0.000352778, // 0.0254 / 72,
+		'pc'=> 0.004233333, //0.0254 / 72 * 12
+		);
+
+	static $duration = array(
+		's'=> 1,
+		'ms'=> 0.001
+		);
+
+
+}
