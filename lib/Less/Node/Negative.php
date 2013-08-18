da4890a66dd163a66c27f4ea570e4a452e9d8e0a
Josh <oyejorge@gmail.com>
2013-08-18 00:33:55 -0600

    take maths further

---
+++ lib/Less/Node/Negative.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Less\Node;
+
+class Negative{
+
+	public $value;
+
+	function __construct($node){
+		$this->value = $node;
+	}
+
+	function toCSS($env){
+		return '-'.$this->value->toCSS($env);
+	}
+
+	function compile($env) {
+		if( count($env->parensStack) ){
+			$ret = new \Less\Node\Operation('*', array( new \Less\Node\Dimension(-1), $this->value ) );
+			return $ret->compile($env);
+		}
+		return new \Less\Node\Negative( $this->value->compile($env) );
+	}
+}
