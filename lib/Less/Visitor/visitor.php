3d6c05656c6709a4775a307424ee5afab617017f
Josh <oyejorge@gmail.com>
2013-08-31 16:09:03 -0600

    one visit function

--- lib/Less/Visitor/visitor.php
+++ lib/Less/Visitor/visitor.php
@@ -2,42 +2,40 @@
 
 class Less_visitor{
 
-	function visit($node){
+	function visit($nodes){
 
-		if( is_array($node) ){
-			$this->visitArray($node);
-			return;
+		if( is_array($nodes) ){
+
+			//check for associative arrays
+			if( $nodes !== array_values($nodes) ){
+				return;
+			}
+		}else{
+			$nodes = array($nodes);
 		}
 
-		if( !@property_exists($node,'type') || !$node->type ){
-			return;
-		}
-
-		$funcName = "visit" . $node->type;
-		if( method_exists($this,$funcName) ){
-			$this->$funcName( $node );
-		}
-
-		$deeper_property = $funcName.'Deeper';
-		if( !isset($this->$deeper_property) && method_exists($node,'accept') ){
-			$node->accept($this);
-		}
-
-		$funcName = $funcName . "Out";
-		if( method_exists($this,$funcName) ){
-			$this->$funcName( $node );
-		}
-	}
-
-	function visitArray( $nodes ){
-
-		//check for associative arrays
-		if( $nodes !== array_values($nodes) ){
-			return;
-		}
 
 		for($i = 0, $len = count($nodes); $i < $len; $i++ ){
-			$this->visit($nodes[$i]);
+			$node = $nodes[$i];
+
+			if( !@property_exists($node,'type') || !$node->type ){
+				return;
+			}
+
+			$funcName = "visit" . $node->type;
+			if( method_exists($this,$funcName) ){
+				$this->$funcName( $node );
+			}
+
+			$deeper_property = $funcName.'Deeper';
+			if( !isset($this->$deeper_property) && method_exists($node,'accept') ){
+				$node->accept($this);
+			}
+
+			$funcName = $funcName . "Out";
+			if( method_exists($this,$funcName) ){
+				$this->$funcName( $node );
+			}
 		}
 	}
 
