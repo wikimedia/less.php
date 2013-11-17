77b97f5d89ac9e8fba9245aab5232b8da735a7e1
Josh <oyejorge@gmail.com>
2013-11-16 23:16:35 -0700

    remove unused file

--- test/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function loadLessClass($className){
-    $fileName = __DIR__.'/../lib/'.str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-    if (file_exists($fileName)) {
-        require_once $fileName;
-    }
-}
-spl_autoload_register('loadLessClass');
