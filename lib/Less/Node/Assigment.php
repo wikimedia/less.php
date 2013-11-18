1dd8882bac8ec4619ad4fa7188a6d29cf96e168e
Josh <oyejorge@gmail.com>
2013-11-17 23:20:06 -0700

    Assignment.php for 1.5

--- lib/Less/Node/Assigment.php
+++ lib/Less/Node/Assigment.php
@@ -1,9 +1,8 @@
 <?php
 
 
-class Less_Tree_Assignment {
+class Less_Tree_Assignment extends Less_Tree{
 
-	//public $type = 'Assignment';
 	private $key;
 	private $value;
 
@@ -12,21 +11,28 @@ class Less_Tree_Assignment {
 		$this->value = $val;
 	}
 
-	/*
 	function accept( $visitor ){
 		$visitor->visit( $this->value );
 	}
-	*/
 
-    public function toCss($env) {
-        return $this->key . '=' . (is_string($this->value) ? $this->value : $this->value->toCSS());
-    }
 
-    public function compile($env) {
+	public function compile($env) {
 		if( Less_Parser::is_method($this->value,'compile') ){
 			return new Less_Tree_Assignment( $this->key, $this->value->compile($env));
-        }
-        return $this;
-    }
+		}
+		return $this;
+	}
 
+	public function genCSS( $env, &$strs ){
+		$this->toCSS_Add( $strs, $this->key . '=' );
+		if( is_string($this->value) ){
+			$this->toCSS_Add( $strs, $this->value );
+		}else{
+			$this->value->genCSS( $env, $strs );
+		}
+	}
+
+	public function toCss($env) {
+		return $this->key . '=' . (is_string($this->value) ? $this->value : $this->value->toCSS());
+	}
 }
