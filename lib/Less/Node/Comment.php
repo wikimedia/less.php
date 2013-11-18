91b8e56c68825874c23d26d13ef61e9880b296f0
Josh <oyejorge@gmail.com>
2013-11-18 00:16:19 -0700

    Comment.php for 1.5

--- lib/Less/Node/Comment.php
+++ lib/Less/Node/Comment.php
@@ -1,23 +1,36 @@
 <?php
 
-class Less_Tree_Comment{
+class Less_Tree_Comment extends Less_Tree{
 
-	//public $type = 'Comment';
+	public function __construct($value, $silent, $index, $currentFileInfo ){
+		$this->value = $value;
+		$this->silent = !! $silent;
+		$this->currentFileInfo = $currentFileInfo;
+	}
 
-    public function __construct($value, $silent)
-    {
-        $this->value = $value;
-        $this->silent = !! $silent;
-    }
+	public function genCSS( $env, &$strs ){
+		if( $this->debugInfo ){
+			//$this->toCSS_Add( $strs, tree.debugInfo($env, $this), $this->currentFileInfo, $this->index);
+		}
+		$this->toCSS_Add( $strs, trim($this->value) );//TODO shouldn't need to trim, we shouldn't grab the \n
+	}
 
-    public function compile($env = null)
-    {
-        return $this;
-    }
+	public function toCSS($env){
+		return $env->compress ? '' : $this->value;
+	}
 
-    public function toCSS($env)
-    {
-        return $env->compress ? '' : $this->value;
-    }
+	public function isSilent( $env ){
+		$isReference = ($this->currentFileInfo && $this->currentFileInfo['reference'] && (!isset($this->isReferenced) || !$this->isReferenced) );
+		$isCompressed = $env->compress && !preg_match('/^\/\*!/', $this->value);
+		return $this->silent || $isReference || $isCompressed;
+	}
+
+	public function compile(){
+		return $this;
+	}
+
+	public function markReferenced(){
+		$this->isReferenced = true;
+	}
 
 }
