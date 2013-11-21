93b347000f2ab93bea85e336ece5a3644c440a1d
Josh <oyejorge@gmail.com>
2013-11-21 10:31:29 -0700

    join-selector-visitor for 1.5

--- lib/Less/Visitor/join-selector-visitor.php
+++ lib/Less/Visitor/join-selector-visitor.php
@@ -13,17 +13,27 @@ class Less_joinSelectorVisitor extends Less_visitor{
 	}
 
 	function visitRuleset($rulesetNode) {
+
 		$context = end($this->contexts); //$context = $this->contexts[ count($this->contexts) - 1];
 		$paths = array();
-		//$this->contexts[] = $paths;
-		if( !$rulesetNode->root ){
-			$rulesetNode->joinSelectors($paths, $context, $rulesetNode->selectors);
-			$rulesetNode->paths = $paths;
-		}
-
-		//array_pop($this->contexts);
 		$this->contexts[] = $paths;
 
+		if( !$rulesetNode->root ){
+
+			$selectors = array();
+			foreach($rulesetNode->selectors as $selector){
+				if( $selector->getIsOutput() ){
+					$selectors[] = $selector;
+				}
+			}
+
+			$rulesetNode->selectors = $selectors
+			if( count($rulesetNode->selectors) === 0 ){
+				$rulesetNode->rules = array();
+			}
+			$rulesetNode->joinSelectors( $paths, $context, $rulesetNode->selectors );
+			$rulesetNode->paths = $paths;
+		}
 	}
 
 	function visitRulesetOut( $rulesetNode ){
@@ -32,7 +42,7 @@ class Less_joinSelectorVisitor extends Less_visitor{
 
 	function visitMedia($mediaNode) {
 		$context = end($this->contexts); //$context = $this->contexts[ count($this->contexts) - 1];
-		$mediaNode->ruleset->root = ( count($context) === 0 || @$context[0]->multiMedia);
+		$mediaNode->rules[0]->root = ( count($context) === 0 || @$context[0]->multiMedia);
 	}
 
 }
