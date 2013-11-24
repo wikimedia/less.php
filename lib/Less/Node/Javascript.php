86950fee808098f002e79cfdccef6dc85416fdb6
Josh <oyejorge@gmail.com>
2013-11-23 22:06:37 -0700

    Javascript.php for 1.5

--- lib/Less/Node/Javascript.php
+++ lib/Less/Node/Javascript.php
@@ -1,8 +1,6 @@
 <?php
 
-class Less_Tree_Javascript{
-
-	//public $type = 'Javascript';
+class Less_Tree_Javascript extends Less_Tree{
 
 	public function __construct($string, $index, $escaped){
 		$this->escaped = $escaped;
@@ -12,6 +10,10 @@ class Less_Tree_Javascript{
 
 	public function compile($env){
 		return $this;
+	}
+
+	function genCSS( $env, &$strs ){
+		self::OutputAdd( $strs, '/* Sorry, can not do JavaScript evaluation in PHP... :( */' );
 	}
 
 	public function toCSS($env = null){
