82df32500430c6175f1654e130044d82ddc8655c
Josh <oyejorge@gmail.com>
2014-01-26 21:10:35 -0700

    version 1.6.1rc1

--- lib/Less/Version.php
+++ lib/Less/Version.php
@@ -2,8 +2,8 @@
 
 class Less_Version{
 
-	const version = '1.5.1.1';			// The current build number of less.php
-	const less_version = '1.5.1';		// The less.js version that this build should be compatible with
-    const cache_version = '1513';		// The parser cache version
+	const version = '1.6.1rc1';			// The current build number of less.php
+	const less_version = '1.6.1';		// The less.js version that this build should be compatible with
+    const cache_version = '1611';		// The parser cache version
 
 }
