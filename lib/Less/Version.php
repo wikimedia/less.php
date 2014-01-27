2b87a643b5a0dab9aaffd5769e15054e5436bd16
Josh <oyejorge@gmail.com>
2014-01-27 10:24:18 -0700

    version 1.5.1.2

--- lib/Less/Version.php
+++ lib/Less/Version.php
@@ -2,8 +2,8 @@
 
 class Less_Version{
 
-	const version = '1.5.1.1';			// The current build number of less.php
+	const version = '1.5.1.2';			// The current build number of less.php
 	const less_version = '1.5.1';		// The less.js version that this build should be compatible with
-    const cache_version = '1513';		// The parser cache version
+    const cache_version = '1514';		// The parser cache version
 
 }
