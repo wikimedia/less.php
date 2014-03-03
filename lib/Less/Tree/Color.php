6e15215362007afb1f95f659959583767237a89e
Josh <oyejorge@gmail.com>
2014-03-02 19:13:32 -0700

    tree_color for 1.7

--- lib/Less/Tree/Color.php
+++ lib/Less/Tree/Color.php
@@ -41,7 +41,15 @@ class Less_Tree_Color extends Less_Tree{
 	}
 
 	public function luma(){
-		return (0.2126 * $this->rgb[0] / 255) + (0.7152 * $this->rgb[1] / 255) + (0.0722 * $this->rgb[2] / 255);
+		$r = $this->rgb[0] / 255;
+		$g = $this->rgb[1] / 255;
+		$b = $this->rgb[2] / 255;
+
+		$r = ($r <= 0.03928) ? $r / 12.92 : pow((($r + 0.055) / 1.055), 2.4);
+		$g = ($g <= 0.03928) ? $g / 12.92 : pow((($g + 0.055) / 1.055), 2.4);
+		$b = ($b <= 0.03928) ? $b / 12.92 : pow((($b + 0.055) / 1.055), 2.4);
+
+		return 0.2126 * $r + 0.7152 * $g + 0.0722 * $b;
 	}
 
 	/**
