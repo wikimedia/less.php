6412f237928a67df13be1a34338343620c880385
Nikolay Yordanov <me@nyordanov.com>
2014-04-13 17:53:25 +0100

    test custom functions

---
+++ test/phpunit/FunctionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+class phpunit_FunctionTest extends phpunit_bootstrap{
+	/**
+	 * Test
+	 */
+	public function testFunction(){
+		echo "\nBegin Tests";
+
+		$less_file = $this->fixtures_dir.'/functions/less/f1.less';
+		$expected_css = file_get_contents( $this->fixtures_dir.'/functions/css/f1.css' );
+
+		$parser = new Less_Parser();
+
+		$parser->registerFunction( 'myfunc-reverse', function( $arg ) {
+			if( is_a( $arg, 'Less_Tree_Quoted' ) ) {
+				$arg->value = strrev( $arg->value );
+				return $arg;
+			}
+		});
+
+		$parser->parseFile( $less_file );
+		$generated_css = $parser->getCss();
+
+		$this->assertEquals( $expected_css, $generated_css );
+	}
+
+}
