d5302bd36f491ae9e6446603be6353f2f429122e
paladox <paladox@users.noreply.github.com>
2015-11-22 11:19:06 +0000

    Update cache_version

--- lib/Less/Version.php
+++ lib/Less/Version.php
@@ -9,7 +9,7 @@
 class Less_Version{
 
 	const version = '1.7.0.10';			// The current build number of less.php
-	const less_version = '1.7.0';		// The less.js version that this build should be compatible with
-	const cache_version = '170';		// The parser cache version
+	const less_version = '1.7.5';			// The less.js version that this build should be compatible with
+	const cache_version = '175';			// The parser cache version
 
 }
