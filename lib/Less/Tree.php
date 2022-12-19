7959450e22ef03092d1e55d1a176974aa5c62351
Rajesh Kumar <glo71317@adobe.com>
2022-12-19 19:18:48 +0530

    Fixed creation of dynamic property Less_Tree_Media::||

--- lib/Less/Tree.php
+++ lib/Less/Tree.php
@@ -9,6 +9,9 @@
 class Less_Tree {
 
 	public $cache_string;
+    public $parensInOp;
+    public $extendOnEveryPath;
+    public $allExtends;
 
 	public function toCSS() {
 		$output = new Less_Output();
