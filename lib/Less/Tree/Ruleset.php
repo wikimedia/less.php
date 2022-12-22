d588bb0e17d4db24865fdaea95d99b8a2ebbb021
Timo Tijhof <krinkle@fastmail.com>
2022-12-22 18:46:51 +0000

    Less_Tree_Ruleset: Fix unused variable (PhanNoopVariable)
    * lib/Less/Tree/Ruleset.php:56
      PhanNoopVariable Unused variable

--- lib/Less/Tree/Ruleset.php
+++ lib/Less/Tree/Ruleset.php
@@ -53,7 +53,7 @@ class Less_Tree_Ruleset extends Less_Tree {
 	public function accept( $visitor ) {
 		if ( $this->paths ) {
 			$paths_len = count( $this->paths );
-			for ( $i = 0,$paths_len; $i < $paths_len; $i++ ) {
+			for ( $i = 0; $i < $paths_len; $i++ ) {
 				$this->paths[$i] = $visitor->visitArray( $this->paths[$i] );
 			}
 		} elseif ( $this->selectors ) {
