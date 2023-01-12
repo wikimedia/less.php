53e9a06aa29edb084c2ecfc35de3c60d604be7d8
Timo Tijhof <krinkle@fastmail.com>
2023-01-12 01:50:27 +0000

    test: Include bug-reports/ fixture in PHPUnit test suite
    These fixtures cover bug reports that upstream lessjs addressed at
    some point, including the lessjs output that goes along with it.
    
    I noticed two failures:
    
    * 110.css: This failed most notably because the "expected" output
      from lessjs was not stored. I generated it using lessjs 4.0.0.
      It still differs slightly, but it's a more useful target now.
    
    * 280.css: This is a known minor difference in how invalid input
      is or isn't corrected.
    
    Skip these as known failures, and include the rest in CI coverage
    going forward.

--- test/phpunit/FixturesTest.php
+++ test/phpunit/FixturesTest.php
@@ -1,49 +1,83 @@
 <?php
 
 class phpunit_FixturesTest extends phpunit_bootstrap {
+	private const KNOWN_FAILURE = [
+		'bug-reports' => [
+			// <lessjs>   background: transparent url("../images/logo.png")
+			// <less.php> background: transparent url("images/logo.png")
+			'110' => true,
+
+			// Invalid input, which lessjs autocorrects.
+			//
+			// <lessjs>   @charset 'UTF-8';
+			// <lessjs>   .test {}
+			// <less.php> .test {}
+			// <less.php> @charset 'UTF-8';
+			'280' => true,
+		],
+	];
 
 	public static function provideFixtures() {
-		foreach ( glob( self::getFixtureDir() . '/lessjs/expected/*.css' ) as $file ) {
-			$name = basename( $file, '.css' );
-			yield "Fixtures/lessjs $name" => [ $file ];
-		}
-		foreach ( glob( self::getFixtureDir() . '/less.php/css/*.css' ) as $file ) {
-			$name = basename( $file, '.css' );
-			yield "Fixtures/less.php $name" => [ $file ];
+		foreach ( [
+			'lessjs' => 'expected',
+			'less.php' => 'css',
+			'bug-reports' => 'css',
+		] as $group => $expectedSubdir ) {
+			$expectedDir = self::getFixtureDir() . "/$group/$expectedSubdir";
+			if ( !is_dir( $expectedDir ) ) {
+				// Check because glob() tolerances non-existence
+				throw new RuntimeException( "Directory missing: $expectedDir" );
+			}
+			foreach ( glob( "$expectedDir/*.css" ) as $cssFile ) {
+				// From /Fixtures/lessjs/css/something.css
+				// into /Fixtures/lessjs/less/name.less
+				$name = basename( $cssFile, '.css' );
+				$lessFile = dirname( dirname( $cssFile ) ) . '/less/' . $name . '.less';
+				if ( self::KNOWN_FAILURE[ $group ][ $name ] ?? false ) {
+					continue;
+				}
+				yield "Fixtures/$group $name" => [ $cssFile, $lessFile ];
+			}
 		}
 	}
 
 	/**
 	 * @dataProvider provideFixtures
 	 */
-	public function testFixture( $cssFile ) {
-		// Translate /Fixtures/lessjs/css/name.css to /Fixtures/lessjs/less/name.less
-		$name = basename( $cssFile, '.css' );
-		$lessFile = dirname( dirname( $cssFile ) ) . '/less/' . $name . '.less';
-		$expected_css = trim( file_get_contents( $cssFile ) );
-
-		$fixtureGroup = basename( dirname( dirname( $cssFile ) ) );
+	public function testFixture( $cssFile, $lessFile ) {
+		$expectedCSS = trim( file_get_contents( $cssFile ) );
 
 		// Check with standard parser
 		$parser = new Less_Parser();
-		$parser->parseFile( $lessFile );
-		$css = $parser->getCss();
+		try {
+			$parser->parseFile( $lessFile );
+			$css = $parser->getCss();
+		} catch ( Less_Exception_Parser $e ) {
+			$css = $e->getMessage();
+		}
 		$css = trim( $css );
-		$this->assertEquals( $expected_css, $css, "Standard compiler" );
+		$this->assertSame( $expectedCSS, $css, "Standard compiler" );
 
 		// Check with cache
 		$options = array( 'cache_dir' => $this->cache_dir );
 		$files = array( $lessFile => '' );
-
-		$css_file_name = Less_Cache::Regen( $files, $options );
-		$css = file_get_contents( $this->cache_dir.'/'.$css_file_name );
+		try {
+			$cacheFile = Less_Cache::Regen( $files, $options );
+			$css = file_get_contents( $this->cache_dir . '/' . $cacheFile );
+		} catch ( Less_Exception_Parser $e ) {
+			$css = $e->getMessage();
+		}
 		$css = trim( $css );
-		$this->assertEquals( $expected_css, $css, "Regenerating cache" );
+		$this->assertEquals( $expectedCSS, $css, "Regenerating cache" );
 
 		// Check using the cached data
-		$css_file_name = Less_Cache::Get( $files, $options );
-		$css = file_get_contents( $this->cache_dir.'/'.$css_file_name );
+		try {
+			$cacheFile = Less_Cache::Get( $files, $options );
+			$css = file_get_contents( $this->cache_dir . '/' . $cacheFile );
+		} catch ( Less_Exception_Parser $e ) {
+			$css = $e->getMessage();
+		}
 		$css = trim( $css );
-		$this->assertEquals( $expected_css, $css, "Using cache" );
+		$this->assertEquals( $expectedCSS, $css, "Using cache" );
 	}
 }
