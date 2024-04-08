wikimedia/less.php

commit a1173e7ea2f54f798b64f6e64cc6dded61095203

Implement ImportVisitor to support interpolated variable imports
The idea of an Import visitor is to got through all the import nodes
and process them. Save all variables to be used by other files by
adding an import root.

The approach used in implementing the import visitor is similar to
Less.js, but last minute while testing I removed the `onImported`
method and everything still worked perfectly. One of the things this
method does is setting `Less_Tree_Import->root` which I moved to be
done inside Less_Tree_Import->ParseImport instead.

Bug: T353133
Change-Id: I8220cd808f631eb0a2fa113e1a7505b674932eda

lib/Less/ImportVisitor.php
@@ -1,0 +1,168 @@
+<?php
+
+class Less_ImportVisitor extends Less_VisitorReplacing {
+
+	public $env;
+	public $variableImports = [];
+
+	public $_currentDepth = 0;
+	public $importItem;
+
+	public function __construct( $env ) {
+		parent::__construct();
+		// NOTE: Upstream creates a new environment/context here. We re-use the main one instead.
+		// This makes Less_Environment->addParsedFile() easier to support (which is custom to Less.php)
+		$this->env = $env;
+		// NOTE: Upstream `importCount` is not here, appears unused.
+		// NOTE: Upstream `isFinished` is not here, we simply call tryRun() once at the end.
+		// NOTE: Upstream `onceFileDetectionMap` is instead Less_Environment->isFileParsed.
+		// NOTE: Upstream `ImportSequencer` logic is directly inside ImportVisitor for simplicity.
+	}
+
+	public function run( &$root ) {
+		$root = $this->visitObj( $root );
+		$this->tryRun();
+	}
+
+	public function visitImport( &$importNode, &$visitDeeper ) {
+		$inlineCSS = $importNode->options['inline'];
+
+		if ( !$importNode->css || $inlineCSS ) {
+
+			$env = clone $this->env;
+			$importParent = $env->frames[0];
+			if ( $importNode->isVariableImport() ) {
+				$this->addVariableImport( [
+					'function' => 'processImportNode',
+					'args' => [ $importNode, $env, $importParent ]
+				] );
+			} else {
+				$this->processImportNode( $importNode, $env, $importParent );
+			}
+		}
+		$visitDeeper = false;
+		return $importNode;
+	}
+
+	public function processImportNode( &$importNode, $env, &$importParent ) {
+		$evaldImportNode = $inlineCSS = $importNode->options['inline'];
+
+		$evaldImportNode = $importNode->compileForImport( $env );
+		// get path & uri
+		$callback = Less_Parser::$options['import_callback'];
+		$path_and_uri = is_callable( $callback ) ? $callback( $evaldImportNode ) : null;
+		if ( !$path_and_uri ) {
+			$path_and_uri = $evaldImportNode->PathAndUri();
+		}
+		if ( $path_and_uri ) {
+			[ $full_path, $uri ] = $path_and_uri;
+		} else {
+			$full_path = $uri = $evaldImportNode->getPath();
+		}
+		'@phan-var string $full_path';
+
+		if ( $evaldImportNode && ( !$evaldImportNode->css || $inlineCSS ) ) {
+
+			if ( ( isset( $importNode->options['multiple'] ) && $importNode->options['multiple'] ) ) {
+				$env->importMultiple = true;
+			}
+
+			for ( $i = 0; $i < count( $importParent->rules ); $i++ ) {
+				if ( $importParent->rules[$i] === $importNode ) {
+					$importParent->rules[$i] = $evaldImportNode;
+					break;
+				}
+			}
+
+			if ( $evaldImportNode->options['inline'] ) {
+				$env->addParsedFile( $full_path );
+				$contents = new Less_Tree_Anonymous( file_get_contents( $full_path ), 0, [], true, true );
+
+				if ( $evaldImportNode->features ) {
+					return new Less_Tree_Media( [ $contents ], $evaldImportNode->features->value );
+				}
+
+				return [ $contents ];
+			}
+
+			// optional (need to be before "CSS" to support optional CSS imports. CSS should be checked only if empty($this->currentFileInfo))
+			if ( isset( $evaldImportNode->options['optional'] ) && $evaldImportNode->options['optional'] && !file_exists( $full_path )
+			&& ( !$evaldImportNode->css || !empty( $evaldImportNode->currentFileInfo ) ) ) {
+				return [];
+			}
+
+			// css ?
+			if ( $evaldImportNode->css ) {
+				$features = ( $evaldImportNode->features ? $evaldImportNode->features->compile( $env ) : null );
+				return new Less_Tree_Import( $evaldImportNode->compilePath( $env ), $features, $evaldImportNode->options, $evaldImportNode->index );
+			}
+
+			$evaldImportNode->ParseImport( $full_path, $uri, $env );
+			$importNode = $evaldImportNode;
+			return $importNode;
+		} else {
+			$this->tryRun();
+		}
+	}
+
+	public function addVariableImport( $callback ) {
+		array_push( $this->variableImports, $callback );
+	}
+
+	public function tryRun() {
+		while ( true ) {
+			if ( count( $this->variableImports ) === 0 ) {
+				break;
+			}
+			$variableImport = $this->variableImports[0];
+
+			$this->variableImports = array_slice( $this->variableImports, 1 );
+			$function = $variableImport['function'];
+
+			$this->$function( ...$variableImport["args"] );
+		}
+	}
+
+	public function visitRule( $ruleNode, &$visitDeeper ) {
+		$visitDeeper = false;
+		return $ruleNode;
+	}
+
+	public function visitDirective( $directiveNode, $visitArgs ) {
+		array_unshift( $this->env->frames, $directiveNode );
+		return $directiveNode;
+	}
+
+	public function visitDirectiveOut( $directiveNode ) {
+		array_shift( $this->env->frames );
+	}
+
+	public function visitMixinDefinition( $mixinDefinitionNode, $visitArgs ) {
+		array_unshift( $this->env->frames, $mixinDefinitionNode );
+		return $mixinDefinitionNode;
+	}
+
+	public function visitMixinDefinitionOut( $mixinDefinitionNode ) {
+		array_shift( $this->env->frames );
+	}
+
+	public function visitRuleset( $rulesetNode, $visitArgs ) {
+		array_unshift( $this->env->frames, $rulesetNode );
+		return $rulesetNode;
+	}
+
+	public function visitRulesetOut( $rulesetNode ) {
+		array_shift( $this->env->frames );
+	}
+
+	public function visitMedia( &$mediaNode, $visitArgs ) {
+		$mediaNode->allExtends = [];
+		array_unshift( $this->env->frames, $mediaNode->allExtends );
+		return $mediaNode;
+	}
+
+	public function visitMediaOut( &$mediaNode ) {
+		array_shift( $this->env->frames );
+	}
+
+}

lib/Less/Tree/Import.php
@@ -12,6 +12,7 @@
  * the file has been fetched, and parsed.
  *
  * @private
+ * @see less-2.5.3.js#Import.prototype
  */
 class Less_Tree_Import extends Less_Tree {
 
@@ -115,6 +116,17 @@ class Less_Tree_Import extends Less_Tree {
 
 		// remove query string and fragment
 		return preg_replace( '/[\?#][^\?]*$/', '', $path );
+	}
+
+	public function isVariableImport() {
+		$path = $this->path;
+		if ( $path instanceof Less_Tree_Url ) {
+			$path = $path->value;
+		}
+		if ( $path instanceof Less_Tree_Quoted ) {
+			return $path->containsVariables();
+		}
+		return true;
 	}
 
 	public function compileForImport( $env ) {
@@ -277,6 +289,7 @@ class Less_Tree_Import extends Less_Tree {
 		$root = $parser->parseFile( $full_path, $uri, true );
 
 		$ruleset = new Less_Tree_Ruleset( null, $root->rules );
+		$this->root = $ruleset;
 		$ruleset->evalImports( $import_env );
 
 		return $this->features ? new Less_Tree_Media( $ruleset->rules, $this->features->value ) : $ruleset->rules;
@@ -289,7 +302,7 @@ class Less_Tree_Import extends Less_Tree {
 	 * @param Less_Environment $env
 	 * @return bool|null
 	 */
-	private function skip( $path, $env ) {
+	public function skip( $path, $env ) {
 		$path = Less_Parser::AbsPath( $path, true );
 
 		if ( $path && $env->isFileParsed( $path ) ) {

lib/Less/Parser.php
@@ -186,6 +186,9 @@ class Less_Parser {
 			$root->root = true;
 			$root->firstRoot = true;
 
+			$importVisitor = new Less_ImportVisitor( $this->env );
+			$importVisitor->run( $root );
+
 			$this->PreVisitors( $root );
 
 			self::$has_extends = false;

lib/Less/Tree/Quoted.php
@@ -36,6 +36,10 @@ class Less_Tree_Quoted extends Less_Tree implements Less_Tree_HasValueProperty {
 		}
 	}
 
+	public function containsVariables() {
+		return preg_match( '/(`([^`]+)`)|@\{([\w-]+)\}/', $this->value );
+	}
+
 	public function compile( $env ) {
 		$value = $this->value;
 		if ( preg_match_all( '/`([^`]+)`/', $this->value, $matches ) ) {

lib/Less/Tree/Ruleset.php
@@ -306,7 +306,18 @@ class Less_Tree_Ruleset extends Less_Tree {
 			if ( $r instanceof Less_Tree_Rule && $r->variable === true ) {
 				$this->_variables[$r->name] = $r;
 			}
+			// when evaluating variables in an import statement, imports have not been eval'd
+			// so we need to go inside import statements.
+			if ( $r instanceof Less_Tree_Import && $r->root ) {
+				$vars = $r->root->variables();
+				foreach ( $vars as $key => $name ) {
+					if ( !empty( $key ) ) {
+						$this->_variables[$key] = $name;
+					}
+				}
+			}
 		}
+		return $this->_variables;
 	}
 
 	/**

lib/Less/Visitor/import.php
@@ -1,137 +1,0 @@
-<?php
-
-/*
-class Less_Visitor_import extends Less_VisitorReplacing{
-
-	public $_visitor;
-	public $_importer;
-	public $importCount;
-
-	function __construct( $evalEnv ){
-		$this->env = $evalEnv;
-		$this->importCount = 0;
-		parent::__construct();
-	}
-
-
-	function run( $root ){
-		$root = $this->visitObj($root);
-		$this->isFinished = true;
-
-		//if( $this->importCount === 0) {
-		//	$this->_finish();
-		//}
-	}
-
-	function visitImport($importNode, &$visitDeeper ){
-		$importVisitor = $this;
-		$inlineCSS = $importNode->options['inline'];
-
-		if( !$importNode->css || $inlineCSS ){
-			$evaldImportNode = $importNode->compileForImport($this->env);
-
-			if( $evaldImportNode && (!$evaldImportNode->css || $inlineCSS) ){
-				$importNode = $evaldImportNode;
-				$this->importCount++;
-				$env = clone $this->env;
-
-				if( (isset($importNode->options['multiple']) && $importNode->options['multiple']) ){
-					$env->importMultiple = true;
-				}
-
-				//get path & uri
-				$path_and_uri = null;
-				if( is_callable(Less_Parser::$options['import_callback']) ){
-					$path_and_uri = call_user_func(Less_Parser::$options['import_callback'],$importNode);
-				}
-
-				if( !$path_and_uri ){
-					$path_and_uri = $importNode->PathAndUri();
-				}
-
-				if( $path_and_uri ){
-					list($full_path, $uri) = $path_and_uri;
-				}else{
-					$full_path = $uri = $importNode->getPath();
-				}
-
-
-				//import once
-				if( $importNode->skip( $full_path, $env) ){
-					return array();
-				}
-
-				if( $importNode->options['inline'] ){
-					//todo needs to reference css file not import
-					//$contents = new Less_Tree_Anonymous($importNode->root, 0, array('filename'=>$importNode->importedFilename), true );
-
-					$this->env->addParsedFile($full_path);
-					$contents = new Less_Tree_Anonymous( file_get_contents($full_path), 0, array(), true );
-
-					if( $importNode->features ){
-						return new Less_Tree_Media( array($contents), $importNode->features->value );
-					}
-
-					return array( $contents );
-				}
-
-
-				// css ?
-				if( $importNode->css ){
-					$features = ( $importNode->features ? $importNode->features->compile($env) : null );
-					return new Less_Tree_Import( $importNode->compilePath( $env), $features, $importNode->options, $this->index);
-				}
-
-				return $importNode->ParseImport( $full_path, $uri, $env );
-			}
-
-		}
-
-		$visitDeeper = false;
-		return $importNode;
-	}
-
-
-	function visitRule( $ruleNode, &$visitDeeper ){
-		$visitDeeper = false;
-		return $ruleNode;
-	}
-
-	function visitDirective($directiveNode, $visitArgs){
-		array_unshift($this->env->frames,$directiveNode);
-		return $directiveNode;
-	}
-
-	function visitDirectiveOut($directiveNode) {
-		array_shift($this->env->frames);
-	}
-
-	function visitMixinDefinition($mixinDefinitionNode, $visitArgs) {
-		array_unshift($this->env->frames,$mixinDefinitionNode);
-		return $mixinDefinitionNode;
-	}
-
-	function visitMixinDefinitionOut($mixinDefinitionNode) {
-		array_shift($this->env->frames);
-	}
-
-	function visitRuleset($rulesetNode, $visitArgs) {
-		array_unshift($this->env->frames,$rulesetNode);
-		return $rulesetNode;
-	}
-
-	function visitRulesetOut($rulesetNode) {
-		array_shift($this->env->frames);
-	}
-
-	function visitMedia($mediaNode, $visitArgs) {
-		array_unshift($this->env->frames, $mediaNode->ruleset);
-		return $mediaNode;
-	}
-
-	function visitMediaOut($mediaNode) {
-		array_shift($this->env->frames);
-	}
-
-}
-*/

test/phpunit/FixturesTest.php
@@ -32,7 +32,6 @@ class phpunit_FixturesTest extends phpunit_bootstrap {
 			'css-guards' => true, // T353144
 			'detached-rulesets' => true, // T353143
 			'import' => true, // T353146
-			'import-interpolation' => true, // TODO
 			'import-reference' => true, // T352862
 			'mixin-args' => true, // T352897
 			'mixins-args' => true, // T352897

commit 9105597e08185b135748d9f6968d5359dbd78783

Fix crash on CSS @import with variable name
Follows-up a1173e7ea2 (I8220cd808f), which introduced this crash by
evaluating import nodes much earlier but lacking a try-catch for cases
that don't need to be compiled by LESS. We missed this in CI because
the upstream urls.less is disabled pending T353147.

Follows-up e7ed93fb3d (Iddd8e41137), which regressed path compilation
such that it outputs filesystem paths where URLs are expected.

Bug: T361035
Change-Id: I302e7527c2b72eebbc26f85fa0ff2f54b5988abc

lib/Less/ImportVisitor.php
@@ -45,11 +45,13 @@ class Less_ImportVisitor extends Less_Visitor {
 	}
 
 	public function processImportNode( $importNode, $env, &$importParent ) {
-		$inlineCSS = $importNode->options['inline'];
+		$evaldImportNode = $inlineCSS = $importNode->options['inline'];
 
-		// TODO: We might need upstream's try-catch here
-		// try { … } catch ( e ) { importNode.css = true; importNode.error = e; }
-		$evaldImportNode = $importNode->compileForImport( $env );
+		try {
+			$evaldImportNode = $importNode->compileForImport( $env );
+		} catch ( Exception $e ) {
+			$importNode->css = true;
+		}
 
 		if ( $evaldImportNode && ( !$evaldImportNode->css || $inlineCSS ) ) {
 

lib/Less/Tree/Import.php
@@ -143,7 +143,7 @@ class Less_Tree_Import extends Less_Tree {
 				$pathValue = $path->value;
 				// Add the base path if the import is relative
 				if ( $pathValue && Less_Environment::isPathRelative( $pathValue ) ) {
-					$path->value = $rootpath . $pathValue;
+					$path->value = $this->currentFileInfo['uri_root'] . $pathValue;
 				}
 			}
 			$path->value = Less_Environment::normalizePath( $path->value );

test/Fixtures/less.php/css/T361035-variable-name.css
@@ -1,0 +1,1 @@
+@import "test.css";

test/Fixtures/less.php/less/T361035-variable-name.less
@@ -1,0 +1,5 @@
+.add_an_import(@file_to_import) {
+	@import "@{file_to_import}";
+}
+
+.add_an_import("test.css");