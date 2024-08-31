0b488cdfaea0baabf6fec256fa447795eeea5bf1
Stefan Warnat <ich@stefanwarnat.de>
2024-08-31 21:33:21 +0000

    Less_Visitor: Support BrianHenryIE/strauss codemod
    The BrianHenryIE/strauss codemod tool, may modify upstream classes
    in-place, to prevent dependency collision, by adding a namespace
    or class prefix. For example, a class will be renamed from Less_Visitor
    to ModA_Less_Visitor. This allows multiple versions of same library
    to be loaded in the same web request.
    
    The previous find-replace approach left behind any namespace or class
    prefix, with MyVendor\Something_Less_Tree_Foo_Bar leading to impossible
    method names like "visitMyVendor\SomethingFooBar".
    
    Change-Id: I30095482d5d5780541bb87f3cb0910c762c30273

--- lib/Less/Visitor.php
+++ lib/Less/Visitor.php
@@ -16,7 +16,21 @@ class Less_Visitor {
 		if ( !$node || !is_object( $node ) ) {
 			return $node;
 		}
-		$funcName = 'visit' . str_replace( [ 'Less_Tree_', '_' ], '', get_class( $node ) );
+
+		// Map a class name like "Less_Tree_Foo_Bar" to method like "visitFooBar".
+		//
+		// We do this by taking the last part of the class name (instead of doing
+		// a find-replace from "Less_Tree" to "visit"), so that we support codemod
+		// tools (such as Strauss and Mozart), which may modify our code in-place
+		// to add a namespace or class prefix.
+		// "MyVendor\Something_Less_Tree_Foo_Bar" should also map to "FooBar".
+		//
+		// https://packagist.org/packages/brianhenryie/strauss
+		// https://packagist.org/packages/coenjacobs/mozart
+		$nodeClassNameParts = explode( 'Less_Tree_', get_class( $node ), 2 );
+		$nodeType = end( $nodeClassNameParts );
+		$funcName = 'visit' . strtr( $nodeType, [ '_' => '', '\\' => '' ] );
+
 		if ( isset( $this->_visitFnCache[$funcName] ) ) {
 			$visitDeeper = true;
 			$newNode = $this->$funcName( $node, $visitDeeper );
