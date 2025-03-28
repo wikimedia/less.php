1ade52ae3d56c0a4a7d0450fca94f02856c46b0e
Umherirrender <umherirrender_de.wp@web.de>
2025-03-28 21:21:00 +0100

    Less_Tree_Selector: Remove unused _css class property
    Usage removed in 38f66ac4f4674304275e9a2eb3abd07bf4450644
    
    Change-Id: I5e580ee10bfb2e26e8430863b8d470f581f14869

--- lib/Less/Tree/Selector.php
+++ lib/Less/Tree/Selector.php
@@ -7,7 +7,6 @@ class Less_Tree_Selector extends Less_Tree {
 	public $elements;
 	public $condition;
 	public $extendList = [];
-	public $_css;
 	public $index;
 	public $evaldCondition = false;
 	public $currentFileInfo = [];
